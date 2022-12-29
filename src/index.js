ddd713f0f7c39f369edb69f5d274acbc9cfa0a19
Artem <artic12101997dr@gmail.com>
2022-12-29 21:53:22 +0200

    final work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
-import createCountryMarkup from "./templates/createCountryMarkup.hbs"
+import createListOfCountry from "./templates/createListOfCountry.hbs";
+import createCountryMarkup from "./templates/createCountryMarkup.hbs";
 
 const DEBOUNCE_DELAY = 300;
 const countryNameInput = document.getElementById("search-box");
@@ -10,46 +11,29 @@ const listOfCountries = document.querySelector(".country-list");
 const oneCountry = document.querySelector(".country-info");
 
 function handleInput(event) {
-	let searchValue = event.target.value
+	let searchValue = event.target.value.trim()
 	fetchCountries(searchValue).then(data => {
-    if (data.length > 10) {
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+	if (data.length > 10) {
+		(oneCountry.innerHTML = "") || (listOfCountries.innerHTML = "");
+		Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.")
         return
     }
     if (data.length >= 2) {
-        const handleCountry = data
-            .map((country) => {
-            return `<li class="country__item"><img class="country__flag" src=${country.flags.svg} alt=${country.name.official}></img>${country.name.common}</li>`}).join("")
         oneCountry.innerHTML = "";
-        listOfCountries.innerHTML = handleCountry;
-        
-
-
-        listOfCountries.style.display = 'flex';
-        listOfCountries.style.flexDirection = 'column';
-        listOfCountries.style.gap = '20px'
-
-        const liEls = listOfCountries.querySelectorAll(".country__item");
-        liEls.forEach((li) => {
-            li.style.listStyle = 'none';
-            li.style.display = 'flex';
-            li.style.alignItems = 'center'
-            li.style.gap = '20px'
-        })
-        const imgEls = listOfCountries.querySelectorAll(".country__flag");
-        imgEls.forEach((img) => {
-            img.style.display = 'block';
-            img.style.height = 'auto';
-            img.style.maxWidth = '50px';
-        })
-        console.log(data);
+		listOfCountries.innerHTML = createListOfCountry(data);
+		Notiflix.Notify.info("Hmmm! What exactly you are looking for..")
     }
     if (data.length === 1) {
         listOfCountries.innerHTML = "";
-        oneCountry.innerHTML = createCountryMarkup(data);        
+		oneCountry.innerHTML = createCountryMarkup(data);
+		Notiflix.Notify.success("We find it!")
     }
     
 	}).catch(err => {
+		if (!searchValue) {
+			(oneCountry.innerHTML = "") || (listOfCountries.innerHTML = "");
+			return
+		}
 		Notiflix.Notify.failure("Oops, there is no country with that name")
 	})
 };
